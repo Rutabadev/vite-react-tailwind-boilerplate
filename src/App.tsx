d0e2db22261b089f32c394471805279376eb8831
etienne.raby <etienne.raby@fittingbox.com>
2021-01-04 17:15:13 +0100

    setup done

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Footer } from './Footer';
+import { Header } from './Header';
+
+function App() {
+   return (
+      <div className="App h-screen flex flex-col">
+         <Header></Header>
+         <main className="flex-grow">
+            <h2>Hello I am main my boy</h2>
+         </main>
+         <Footer></Footer>
+      </div>
+   );
+}
+
+export default App;
---
+++ src/Footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Footer = () => (
+   <footer className="bg-gray-800 text-lg text-gray-100">
+      This is the footer
+   </footer>
+);
